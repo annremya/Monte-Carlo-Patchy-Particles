0a20eded32684bc223ae58f479fd85fdb6a13faf
Remya Mathews <40625281+annremya@users.noreply.github.com>
2025-01-27 13:19:19 +0530

    Random Initial Configuration

---
+++ MC_NPT_IPC_2D/Random_Initial_Configuration/par.h
@@ -0,0 +1,30 @@
+!     Control parameters      
+      Integer  nsample, nadj, nxyze, nxyzp, nsamplep
+      Integer*8  eqcycle, procycle
+      Integer nann
+      Parameter (eqcycle=3d+8, procycle=1d+6)
+      Parameter (nsample = 250000, nadj = 25000, nsamplep = 10 )
+      Parameter (nann = 1, nxyze = 2000000, nxyzp = 2000)
+!     Simulation parameters	
+      Double precision  pres, rcut, T0, pi, alpha
+      Parameter(pres = 0.01d0, rcut = 2.0d0, alpha = 1.0)
+      Parameter (T0 = 1.0d0, pi = 3.14159d0)
+
+!     Potential parameters	
+      Double precision sig, zeta, kappa, bjerrum
+      Double precision epsiln1, deg, epsiln2
+      Double precision elp, elpnp, elnp
+      Parameter (sig = 1.0d0)
+      Parameter (zeta = 0.0d0, kappa = 0.0d0, bjerrum = 0.28d0)
+      Parameter (elp = 1.5d0, elpnp = 1.28d0,elnp = 1.46d0)
+	Parameter (epsiln1 = -2.0d0,epsiln2 = 2.0d0)
+	Parameter (deg = 90.0d0)  !patch coverage = 50
+      Logical restart
+      Parameter (restart = .false.)       
+
+! nann             : no. of annealing (Temperature) steps
+! bjerrum          : Bjerrum length
+! alpha            : cooling-schedule factor
+!patch coverage % = 1 - cos(deg)/2
+!deg = patch angle in deg
+
